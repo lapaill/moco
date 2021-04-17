88f2f53cc5880f1aeedf18ed1c780f16b468889d
Joseph Paillard <joseph.paillard@mines-paristech.fr>
2021-04-17 15:40:35 +0200

    add launch.sh for slurm

---
+++ launch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=8051_mocoresnet18 
+#SBATCH -p gpu-cbio
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task 12
+#SBATCH --mem 100000
+#SBATCH --output=slurm_out/Hflip-Vflip-GrayScale-GaussianBlur-Rotate90-Jitter-200.out  # Nom du fichier de sortie contenant l'ID et l'indice 
+#SBATCH --error=slurm_out/Hflip-Vflip-GrayScale-GaussianBlur-Rotate90-Jitter-200.err   # Nom du fichier d'erreur (ici commun avec la sortie) 
+
+module load cuda10.0
+python /mnt/data4/jpaillard/projets/moco/main_moco_histo.py \
+    -a resnet18 \
+    --mlp \
+    --moco-t 0.2 \
+    --cos \
+    --lr 0.003 \
+    --batch-size 4096 \
+    --data /mnt/data4/tlazard/data/dataset_stage_2/data \
+    --rank 0 \
+    --world-size 1 \
+    --multiprocessing-distributed \
+    --dist-url "tcp://127.0.0.1:8051" \
+    --name_expe Hflip-Vflip-GrayScale-GaussianBlur-Rotate90-Jitter-200 \
+    --epochs 200 \
+    --crop-and-transform \
+    --transformations Hflip Vflip GrayScale GaussianBlur ElasticDistorsion Rotate90 Jitter
+#    transformations [ Hflip, Vflip, Crop, Crop_and_rotate, HEaug, GrayScale, ElasticDistorsion, GaussianBlur, Jitter, Rotate90 ]
+#   /!\ Ne pas mettre d'espace après les '\' de retour à la ligne 
